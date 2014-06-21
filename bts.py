34bd972e68e2c3ede367b64d1967cdfd7ad53311
Nicolas Wack <wackou@gmail.com>
2014-06-21 14:44:08 +0200

    Config file is now a JSON file

--- bts.py
+++ bts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from os.path import join, dirname, exists, islink
+from os.path import join, dirname, exists, islink, expanduser
 from subprocess import Popen, PIPE
 from collections import namedtuple
 from argparse import RawTextHelpFormatter
@@ -9,30 +9,47 @@ import argparse
 import os
 import sys
 import shutil
+import json
 
 IOStream = namedtuple('IOStream', 'status, stdout, stderr')
 
 
 # load config
-config_file = join(dirname(__file__), 'config.py')
-exec(open(config_file).read())
+config = json.load(open(join(dirname(__file__), 'config.json')))
+try:
+    config = config[sys.platform]
+except KeyError:
+    raise OSError('OS not supported yet, please submit a patch :)')
 
+# expand '~' in path names to the user's home dir
+for attr, path in config.items():
+    config[attr] = expanduser(path)
+
+
+BITSHARES_BUILD_DIR = config['BITSHARES_BUILD_DIR']
+BITSHARES_HOME_DIR = config['BITSHARES_HOME_DIR']
+BITSHARES_BIN_DIR = config['BITSHARES_BIN_DIR']
 
 # on mac osx, readline needs to be installed by brew and
-# "brew link --force readline" to take precedence over the outdated readline
+# "brew link --force readline" to take precedence over the
+# outdated version of the system
 
 # parse commandline args
 DESC="""following commands are available:
- - clean      : clean build directory
- - build      : update and build bts client
- - run [hash] : run latest compiled bts client, or the one with the given hash
- - list_bins  : list installed bitshares client binaries
+ - clean_homedir : clean home directory. WARNING: this will delete your wallet!
+ - clean         : clean build directory
+ - build [hash]  : update and build bts client
+ - run [hash]    : run latest compiled bts client, or the one with the given hash
+ - list_bins     : list installed bitshares client binaries
 """
 parser = argparse.ArgumentParser(description=DESC,
                                  formatter_class=RawTextHelpFormatter)
-parser.add_argument('command', help='the command to run',
-                    choices=['build', 'run', 'clean', 'list'])
-parser.add_argument('hash', help='the hash of the desired commit', nargs='?')
+parser.add_argument('command', choices=['clean_homedir', 'clean', 'build', 'run', 'list'],
+                    help='the command to run')
+parser.add_argument('-r', '--rpc', action='store_true',
+                    help='run binary with RPC server activated')
+parser.add_argument('hash',
+                    help='the hash of the desired commit', nargs='?')
 args = parser.parse_args()
 
 
@@ -46,8 +63,11 @@ def _run(cmd, io=False):
             stdout, stderr = (str(stdout, encoding='utf-8'),
                               str(stderr, encoding='utf-8'))
         return IOStream(p.returncode, stdout, stderr)
+
     else:
-        return IOStream(os.system(cmd), None, None)
+        p = Popen(cmd, shell=True)
+        p.communicate()
+        return IOStream(p.returncode, None, None)
 
 def run(cmd, io=False):
     r = _run(cmd, io)
@@ -57,14 +77,13 @@ def run(cmd, io=False):
 
 
 def clone():
-    if not exists(Config.BITSHARES_BUILD_DIR):
-        run('git clone https://github.com/BitShares/bitshares_toolkit.git "%s"' % Config.BITSHARES_BUILD_DIR)
-        os.chdir(Config.BITSHARES_BUILD_DIR)
+    if not exists(BITSHARES_BUILD_DIR):
+        run('git clone https://github.com/BitShares/bitshares_toolkit.git "%s"' % BITSHARES_BUILD_DIR)
+        os.chdir(BITSHARES_BUILD_DIR)
         run('git submodule init')
 
 
-
-update = lambda: run('git pull && git submodule update')
+update = lambda: run('git checkout master && git pull && git submodule update')
 clean_config = lambda: run('rm -f CMakeCache.txt')
 
 if sys.platform == 'darwin':
@@ -91,18 +110,18 @@ def install_last_built_bin():
             result = join(dirname(src), os.readlink(src))
             print('Following symlink %s -> %s' % (src, result))
             src = result
-        dst = join(Config.BITSHARES_BIN_DIR, dst)
+        dst = join(BITSHARES_BIN_DIR, dst)
         shutil.copy(src, dst)
         return dst
 
-    if not exists(Config.BITSHARES_BIN_DIR):
-        os.makedirs(Config.BITSHARES_BIN_DIR)
+    if not exists(BITSHARES_BIN_DIR):
+        os.makedirs(BITSHARES_BIN_DIR)
 
-    client = join(Config.BITSHARES_BUILD_DIR, 'programs', 'client', 'bitshares_client')
+    client = join(BITSHARES_BUILD_DIR, 'programs', 'client', 'bitshares_client')
 
     c = install(client, bin_filename)
 
-    last_installed = join(Config.BITSHARES_BIN_DIR, 'bitshares_client')
+    last_installed = join(BITSHARES_BIN_DIR, 'bitshares_client')
     try:
         os.unlink(last_installed)
     except:
@@ -113,8 +132,10 @@ def install_last_built_bin():
 if args.command == 'build':
     clone()
 
-    os.chdir(Config.BITSHARES_BUILD_DIR)
+    os.chdir(BITSHARES_BUILD_DIR)
     update()
+    if args.hash:
+        run('git checkout %s && git submodule update' % args.hash)
     clean_config()
     configure()
     build()
@@ -125,24 +146,24 @@ elif args.command == 'run':
     if args.hash:
         # if git rev specified, runs specific version
         print('Running specific instance of the bts client: %s' % args.hash)
-        bin_name = run('ls %s' % join(Config.BITSHARES_BIN_DIR,
+        bin_name = run('ls %s' % join(BITSHARES_BIN_DIR,
                                       'bitshares_client_*%s*' % args.hash),
                        io=True).stdout.strip()
     else:
         # run latest version
-        bin_name = join(Config.BITSHARES_BIN_DIR, 'bitshares_client')
+        bin_name = join(BITSHARES_BIN_DIR, 'bitshares_client')
 
-    if not exists(Config.BITSHARES_HOME_DIR):
-        # run without --server the first time as we don't have a config.json yet
-        run('"%s" %s' % (bin_name,
-                         '--maximum-number-of-connections=128'))
-    else:
-        run('"%s" %s %s' % (bin_name,
-                            '--maximum-number-of-connections=128',
-                            '--server'))
+    run_args = ['--maximum-number-of-connections=128']
+    if args.rpc:
+        run_args.append('--server')
+
+    run([bin_name] + run_args)
 
 elif args.command == 'clean':
-    run('rm -fr "%s"' % Config.BITSHARES_BUILD_DIR)
+    run('rm -fr "%s"' % BITSHARES_BUILD_DIR)
+
+elif args.command == 'clean_homedir':
+    run('rm -fr "%s"' % BITSHARES_HOME_DIR)
 
 elif args.command == 'list':
-    run('ls -ltr "%s"' % Config.BITSHARES_BIN_DIR)
+    run('ls -ltr "%s"' % BITSHARES_BIN_DIR)
